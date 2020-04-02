d157b5817f42e2e9e2c5bedc0e8d660797309690
Tom de Vries <tom@hackerone.com>
2020-04-02 15:43:40 +0200

    IAM support

--- test/support/integration/plugins/lookup/hashi_vault.py
+++ test/support/integration/plugins/lookup/hashi_vault.py
@@ -1,3 +1,17 @@
+# March 18, 2020
+#
+# Comment author: @tomdev
+#
+# The "hashi_vault" lookup plugin does not support instance profile authentication.
+# Work has been put into this: https://github.com/ansible/ansible/pull/46028
+#
+# That is the base for hashi_vault_iam_support.py
+#
+# Modifications made:
+#   - use hvac.auth.aws.iam_login over the deprecated hvac.auth_aws_iam login method
+#   - don't provide region as argument on login, see: https://hvac.readthedocs.io/en/stable/usage/auth_methods/aws.html#caveats-for-non-default-aws-regions
+#
+
 # (c) 2015, Jonathan Davila <jonathan(at)davila.io>
 # (c) 2017 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
@@ -6,7 +20,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = """
-  lookup: hashi_vault
+  lookup: hashi_vault_iam_support
   author: Jonathan Davila <jdavila(at)ansible.com>
   version_added: "2.0"
   short_description: retrieve secrets from HashiCorp's vault
@@ -42,6 +56,29 @@ DOCUMENTATION = """
       description: Secret id for a vault AppRole auth.
       env:
         - name: VAULT_SECRET_ID
+    access_key:
+      description: AWS access key id for a vault AWS IAM auth
+      env:
+        - name: AWS_ACCESS_KEY_ID
+    secret_key:
+      description: AWS secret access key for a vault AWS IAM auth
+      env:
+        - name: AWS_SECRET_ACCESS_KEY
+    session_token:
+      description: Optional AWS IAM session token for a vault AWS IAM auth
+    header_value:
+      description: Optional header value for a vault AWS IAM auth
+    role:
+      description: Role name for a vault AWS IAM auth or vault AWS EC2 auth
+      env:
+        - name: VAULT_ROLE
+    region:
+      description: Optional AWS region for a vault AWS IAM auth
+      env:
+        - name: AWS_DEFAULT_REGION
+      default: us-east-1
+    nonce:
+      description: Optional nonce for a vault AWS EC2 auth
     auth_method:
       description:
       - Authentication method to be used.
@@ -52,9 +89,10 @@ DOCUMENTATION = """
         - userpass
         - ldap
         - approle
+        - aws_iam
+        - aws_ec2
     mount_point:
-      description: vault mount point, only required if you have a custom mount point.
-      default: ldap
+      description: vault mount point for auth_method, only required if you have a custom mount point.
     ca_cert:
       description: path to certificate to use for authentication.
       aliases: [ cacert ]
@@ -69,43 +107,39 @@ DOCUMENTATION = """
 
 EXAMPLES = """
 - debug:
-    msg: "{{ lookup('hashi_vault', 'secret=secret/hello:value token=c975b780-d1be-8016-866b-01d0f9b688a5 url=http://myvault:8200')}}"
-
+    msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hello:value token=c975b780-d1be-8016-866b-01d0f9b688a5 url=http://myvault:8200')}}"
 - name: Return all secrets from a path
   debug:
-    msg: "{{ lookup('hashi_vault', 'secret=secret/hello token=c975b780-d1be-8016-866b-01d0f9b688a5 url=http://myvault:8200')}}"
-
+    msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hello token=c975b780-d1be-8016-866b-01d0f9b688a5 url=http://myvault:8200')}}"
 - name: Vault that requires authentication via LDAP
   debug:
-      msg: "{{ lookup('hashi_vault', 'secret=secret/hello:value auth_method=ldap mount_point=ldap username=myuser password=mypas url=http://myvault:8200')}}"
-
+      msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hello:value auth_method=ldap mount_point=ldap username=myuser password=mypas url=http://myvault:8200')}}"
 - name: Vault that requires authentication via username and password
   debug:
-      msg: "{{ lookup('hashi_vault', 'secret=secret/hello:value auth_method=userpass username=myuser password=mypas url=http://myvault:8200')}}"
-
+      msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hello:value auth_method=userpass username=myuser password=mypas url=http://myvault:8200')}}"
 - name: Using an ssl vault
   debug:
-      msg: "{{ lookup('hashi_vault', 'secret=secret/hola:value token=c975b780-d1be-8016-866b-01d0f9b688a5 url=https://myvault:8200 validate_certs=False')}}"
-
+      msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hola:value token=c975b780-d1be-8016-866b-01d0f9b688a5 url=https://myvault:8200 validate_certs=False')}}"
 - name: using certificate auth
   debug:
-      msg: "{{ lookup('hashi_vault', 'secret=secret/hi:value token=xxxx-xxx-xxx url=https://myvault:8200 validate_certs=True cacert=/cacert/path/ca.pem')}}"
-
+      msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hi:value token=xxxx-xxx-xxx url=https://myvault:8200 validate_certs=True cacert=/cacert/path/ca.pem')}}"
 - name: authenticate with a Vault app role
   debug:
-      msg: "{{ lookup('hashi_vault', 'secret=secret/hello:value auth_method=approle role_id=myroleid secret_id=mysecretid url=http://myvault:8200')}}"
-
+      msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hello:value auth_method=approle role_id=myroleid secret_id=mysecretid url=http://myvault:8200')}}"
 - name: Return all secrets from a path in a namespace
   debug:
-    msg: "{{ lookup('hashi_vault', 'secret=secret/hello token=c975b780-d1be-8016-866b-01d0f9b688a5 url=http://myvault:8200 namespace=teama/admins')}}"
-
+    msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hello token=c975b780-d1be-8016-866b-01d0f9b688a5 url=http://myvault:8200 namespace=teama/admins')}}"
+- name: authenticate via AWS IAM auth
+  debug:
+      msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hello:value auth_method=aws_iam access_key=access secret_key=secret role=myrole url=http://myvault:8200')}}"
+  debug:
+- name: authenticate via AWS EC2 auth
+      msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/hello:value auth_method=aws_ec2 nonce=my_nonce role=myawsrole url=http://myvault:8200')}}"
 # When using KV v2 the PATH should include "data" between the secret engine mount and path (e.g. "secret/data/:path")
 # see: https://www.vaultproject.io/api/secret/kv/kv-v2.html#read-secret-version
 - name: Return latest KV v2 secret from path
   debug:
-    msg: "{{ lookup('hashi_vault', 'secret=secret/data/hello token=my_vault_token url=http://myvault_url:8200') }}"
-
-
+    msg: "{{ lookup('hashi_vault_iam_support', 'secret=secret/data/hello token=my_vault_token url=http://myvault_url:8200') }}"
 """
 
 RETURN = """
@@ -115,6 +149,7 @@ _raw:
 """
 
 import os
+import requests
 
 from ansible.errors import AnsibleError
 from ansible.module_utils.parsing.convert_bool import boolean
@@ -134,17 +169,17 @@ if os.getenv('VAULT_ADDR') is not None:
     ANSIBLE_HASHI_VAULT_ADDR = os.environ['VAULT_ADDR']
 
 
-class HashiVault:
+class HashiVaultIamSupport:
     def __init__(self, **kwargs):
 
         self.url = kwargs.get('url', ANSIBLE_HASHI_VAULT_ADDR)
         self.namespace = kwargs.get('namespace', None)
-        self.avail_auth_method = ['approle', 'userpass', 'ldap']
+        self.avail_auth_method = ['approle', 'userpass', 'ldap', 'aws_iam', 'aws_ec2']
 
         # split secret arg, which has format 'secret/hello:value' into secret='secret/hello' and secret_field='value'
         s = kwargs.get('secret')
         if s is None:
-            raise AnsibleError("No secret specified for hashi_vault lookup")
+            raise AnsibleError("No secret specified for hashi_vault_iam_support lookup")
 
         s_f = s.rsplit(':', 1)
         self.secret = s_f[0]
@@ -194,7 +229,7 @@ class HashiVault:
                 self.client = hvac.Client(url=self.url, token=self.token, verify=self.verify)
 
         if not self.client.is_authenticated():
-            raise AnsibleError("Invalid Hashicorp Vault Token Specified for hashi_vault lookup")
+            raise AnsibleError("Invalid Hashicorp Vault Token Specified for hashi_vault_iam_support lookup")
 
     def get(self):
         data = self.client.read(self.secret)
@@ -212,13 +247,13 @@ class HashiVault:
             pass
 
         if data is None:
-            raise AnsibleError("The secret %s doesn't seem to exist for hashi_vault lookup" % self.secret)
+            raise AnsibleError("The secret %s doesn't seem to exist for hashi_vault_iam_support lookup" % self.secret)
 
         if self.secret_field == '':
             return data['data']
 
         if self.secret_field not in data['data']:
-            raise AnsibleError("The secret %s does not contain the field '%s'. for hashi_vault lookup" % (self.secret, self.secret_field))
+            raise AnsibleError("The secret %s does not contain the field '%s'. for hashi_vault_iam_support lookup" % (self.secret, self.secret_field))
 
         return data['data'][self.secret_field]
 
@@ -247,7 +282,7 @@ class HashiVault:
         if mount_point is None:
             mount_point = 'ldap'
 
-        self.client.auth.ldap.login(username, password, mount_point=mount_point)
+        self.client.auth_ldap(username, password, mount_point=mount_point)
 
     def boolean_or_cacert(self, validate_certs, cacert):
         validate_certs = boolean(validate_certs, strict=False)
@@ -271,11 +306,69 @@ class HashiVault:
 
         self.client.auth_approle(role_id, secret_id)
 
+    def auth_aws_iam(self, **kwargs):
+        access_key = kwargs.get('access_key')
+        secret_key = kwargs.get('secret_key')
+        session_token = kwargs.get('session_token')
+
+        region = kwargs.get('region', os.environ.get('AWS_DEFAULT_REGION'))
+        if region is None:
+            region = 'us-east-1'
+
+        if access_key is None or secret_key is None:
+            import boto3
+            creds = boto3.Session(region_name=region).get_credentials()
+            if creds is None:
+                raise AnsibleError("Authentication method aws iam requires AWS credentials")
+            access_key, secret_key, session_token = creds.access_key, creds.secret_key, creds.token
+
+        header_value = kwargs.get('header_value')
+
+        mount_point = kwargs.get('mount_point')
+        if mount_point is None:
+            mount_point = 'aws'
+
+        role = kwargs.get('role', os.environ.get('VAULT_ROLE', None))
+        if role is None:
+            raise AnsibleError("Authentication method aws iam requires a role")
+
+        #
+        # Change: Removed the region to prevent the following error:
+        #
+        #     hvac.exceptions.InvalidRequest: error making upstream request: received error code 403 from STS: <ErrorResponse xmlns="https://sts.amazonaws.com/doc/2011-06-15/">
+        #
+        #     <Code>SignatureDoesNotMatch</Code>
+        #     <Message>Credential should be scoped to a valid region, not 'us-west-2'. </Message>
+        #
+        # Ref: https://hvac.readthedocs.io/en/stable/usage/auth_methods/aws.html#caveats-for-non-default-aws-regions
+        #
+        self.client.auth.aws.iam_login(access_key, secret_key, session_token=session_token, header_value=header_value, mount_point=mount_point, role=role)
+
+    def auth_aws_ec2(self, **kwargs):
+        url = "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7"
+
+        try:
+            pkcs7 = requests.get(url, timeout=0.1).text.replace('\n', '')
+        except requests.exceptions.ConnectionError:
+            raise AnsibleError("hashi_vault_iam_support lookup plugin failed to connect to http://169.254.169.254, Make sure you are running from a AWS EC2 instance")
+
+        nonce = kwargs.get('nonce')
+
+        role = kwargs.get('role', os.environ.get('VAULT_ROLE', None))
+        if role is None:
+            raise AnsibleError("Authentication method aws ec2 requires a role")
+
+        mount_point = kwargs.get('mount_point')
+        if mount_point is None:
+            mount_point = 'aws'
+
+        self.client.auth_ec2(pkcs7, nonce, role, mount_point=mount_point)
+
 
 class LookupModule(LookupBase):
     def run(self, terms, variables=None, **kwargs):
         if not HAS_HVAC:
-            raise AnsibleError("Please pip install hvac to use the hashi_vault lookup module.")
+            raise AnsibleError("Please pip install hvac to use the hashi_vault_iam_support lookup module.")
 
         vault_args = terms[0].split()
         vault_dict = {}
@@ -285,14 +378,14 @@ class LookupModule(LookupBase):
             try:
                 key, value = param.split('=')
             except ValueError:
-                raise AnsibleError("hashi_vault lookup plugin needs key=value pairs, but received %s" % terms)
+                raise AnsibleError("hashi_vault_iam_support lookup plugin needs key=value pairs, but received %s" % terms)
             vault_dict[key] = value
 
         if 'ca_cert' in vault_dict.keys():
             vault_dict['cacert'] = vault_dict['ca_cert']
             vault_dict.pop('ca_cert', None)
 
-        vault_conn = HashiVault(**vault_dict)
+        vault_conn = HashiVaultIamSupport(**vault_dict)
 
         for term in terms:
             key = term.split()[0]
